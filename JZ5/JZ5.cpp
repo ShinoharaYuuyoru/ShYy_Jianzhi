c2bb21b2cac0a5b64204fc7232e68e76a111dda5
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2020-07-11 18:15:08 +0900

    Update JZ5.cpp

--- JZ5/JZ5.cpp
+++ JZ5/JZ5.cpp
@@ -28,3 +28,30 @@ private:
     stack<int> stack1;
     stack<int> stack2;
 };
+
+
+// Optimize
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+
+        int retNum = stack2.top();
+        stack2.pop();
+        return retNum;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
