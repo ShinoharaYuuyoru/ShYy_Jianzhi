8012ec9f9cfe93083c5d55c438d9506cdbe36619
ShinoharaYuuyoru <ShinoharaYuuyoru@gmail.com>
2020-07-27 02:54:20 +0900

    Create JZ20.cpp

---
+++ JZ20/JZ20.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void push(int value) {
+
+        S.push(value);
+        if (minS.empty() == true) {
+            minS.push(value);
+        }
+        else {
+            int nowMin = minS.top();
+            if (value <= nowMin) {
+                minS.push(value);
+            }
+        }
+    }
+    void pop() {
+        int popVal = S.top();
+        S.pop();
+        if (popVal == minS.top()) {
+            minS.pop();
+        }
+    }
+    int top() {
+        return S.top();
+    }
+    int min() {
+        return minS.top();
+    }
+
+private:
+    stack<int> S;
+    stack<int> minS;
+};
